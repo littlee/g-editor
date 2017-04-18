809a8b83c620e67ee78be08baf0760779a50085f
Carrey Lee <xkli@gizwits.com>
2017-04-18 17:24:28 +0800

    blank editor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Editor, EditorState } from 'draft-js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty()
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Editor editorState={this.state.editorState} onChange={this.onChange}/>
+    )
+  }
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState
+    })
   }
 }
 
-export default App;
+export default App
